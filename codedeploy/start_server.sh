b4e4b0c820827d992a724905eaba33792b56a287
Ubuntu <ubuntu@ip-172-31-12-164.us-east-2.compute.internal>
2024-06-23 16:08:11 +0000

    update

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # source /home/ec2-user/web/.env
 
-cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yaml up -d
+cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
